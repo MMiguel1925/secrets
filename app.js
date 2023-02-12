751f64be2cc80cf32e60c665599242c9f39f0665
Red Dust <comanche.red.dust@gmail.com>
2023-02-12 20:40:45 +0000

    req.logout is asynchronous

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 //jshint esversion:6
 require("dotenv").config();
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require(`ejs`);
+const _ = require(`lodash`);
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const app = express();
 
-mongoose.set("strictQuery", false);
+app.use(express.json()); // to support JSON-encoded bodies
+
+app.use(express.static(`public`));
+app.set(`view engine`, `ejs`);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: false }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //const uri = `${process.env.SERVIDOR_BD}://${process.env.UTILIZADOR}:${process.env.PALAVRA_CHAVE}@${process.env.CLUSTER}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
-
 //ligacao a server remoto: MongoAtlas ou localhost
-
+mongoose.set("strictQuery", false);
 mongoose.connect(`mongodb://127.0.0.1:27017/userDB`, { useNewUrlParser: true });
 
 console.log(`Sucesso a ligar a BD`);
@@ -22,26 +41,16 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-// const secret = process.env.SECRET_DB;
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(passportLocalMongoose);
 
-const User = mongoose.model(`User`, userSchema);
+const User = mongoose.model("User", userSchema);
 
-const express = require("express");
+// passport local mongoose: as 3 linhas abaixo só sao necesseaias qd usamos a sessao
+passport.use(User.createStrategy());
 
-const bodyParser = require("body-parser");
-
-const ejs = require(`ejs`);
-
-const _ = require(`lodash`);
-
-const app = express();
-
-app.set(`view engine`, `ejs`);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(`public`));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// **********************************************
 
 const port = process.env.LOCALHOST_PORT;
 
@@ -54,44 +63,51 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register", { messageError: null });
+});
+
+app.get("/secrets", (req, res) => {
+  res.set("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0");
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", (req, res, next) => {
+  console.log("Session exit..");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    // Store hash in your password DB.
-    if (!err) {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash,
-      });
-      newUser.save((err) => {
-        if (err) return console.log(err);
-        res.send("User registado com sucesso");
-      });
+  User.register({ username: req.body.username }, req.body.password, (err, user) => {
+    if (err) {
+      res.render("register", { messageError: "Message: " + err.message });
+      console.log("ERRO user duplicado: " + err.name);
     } else {
-      res.send("Erro no registo do user!");
+      console.log("Successefuly user register");
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
 
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, (err, foundUser) => {
+app.post("/login", passport.authenticate("local", { failureRedirect: "/", successRedirect: "/secrets", failureMessage: true }), (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, (err) => {
     if (err) {
       console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-          if (result === true) {
-            res.render("secrets");
-          } else {
-            res.send("Login failed");
-          }
-        });
-      }
+      console.log("user login failed");
     }
   });
 });
